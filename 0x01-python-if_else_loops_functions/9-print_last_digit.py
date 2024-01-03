3fe3c03e7179caf024c6b253fad10a008765eb44
opokuamaning <princeopokuamaning@gmail.com>
2024-01-03 12:53:55 -0800

    checked for pycodestyle compliance

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     print(abs(number) % 10, end='')
-    return(abs(number) % 10)
+    return (abs(number) % 10)
 
 #    if number < 0:
 #        number = number * -1

1b18b218c783274ac70d780f2b7df6f31dcc4a2f
opokuamaning <princeopokuamaning@gmail.com>
2024-01-06 19:24:59 -0800

    Modified 1-element.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or id >= len(my_list)::
         return None
     else:
         return my_list[idx]
-    if id >= len(my_list):
-        return None
-    else:
-        return my_list(idx)

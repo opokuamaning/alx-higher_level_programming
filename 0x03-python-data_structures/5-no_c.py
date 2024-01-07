683072981faac7f623259bf4766cf143381e3423
opokuamaning <princeopokuamaning@gmail.com>
2024-01-07 10:44:32 -0800

    Modified 5-mo_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    str_len = len(my_string)
-
-    j = 0
-    new_string = my_string[:]
-
-    for i in range(str_len):
-        if(my_string[i] == 'C' or my_string[i] == 'c'):
-            new_string = new_string[:(i - j)] + my_string[(i + 1):]
-            j += 1
-    return (new_string)
+    return ("".join(c for c in my_string if c not in "Cc"))

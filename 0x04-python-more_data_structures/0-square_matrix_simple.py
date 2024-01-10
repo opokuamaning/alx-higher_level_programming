4ea6792c43d45e50b61c5897d5cfb6df5e2c165f
opokuamaning <princeopokuamaning@gmail.com>
2024-01-10 11:05:02 -0800

    Modified 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same input size
-    new_matrix = [[0 for _ in row] for row in matrix]
-
-    # iterate over the rows and cols of the matrix
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            # Compute rhe square of each matrix
-            new_matrix[i][j] = matrix[i][j] ** 2
-
-    return new_matrix
+    if matrix is not None:
+        new_matrix = []
+        for rows in matrix:
+            new_matrix.append(list(map(lambda x: x**2, rows)))
+        return (new_matrix)
+    return None

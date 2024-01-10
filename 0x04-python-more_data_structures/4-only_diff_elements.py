cf783848041223a52632f250a7cacc3650a1c90d
opokuamaning <princeopokuamaning@gmail.com>
2024-01-10 12:59:25 -0800

    Modified 4-onlt_diff-element.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    only_diff = set_1.copy()
+    diff_set = set_1.symmetric_difference(set_2)
 
-    for element in set_1:
-        if element not in set_2:
-            only_diff.add(element)
-
-    for element in set_2:
-        if element not in set_1:
-            only_diff.add(element)
-    return only_diff
+    return diff_set

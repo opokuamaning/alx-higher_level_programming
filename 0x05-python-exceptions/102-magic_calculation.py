8d10e3089bf3482494829d46de00f6615427e0c5
opokuamaning <princeopokuamaning@gmail.com>
2024-01-22 17:59:43 -0800

    ?Added

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -13,4 +13,16 @@ def magic_calculation(a, b):
     return result
 
 #import dis
-#dis.dis(magic_calculation)
+#dis.dis(magic_calculation)#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return (result)

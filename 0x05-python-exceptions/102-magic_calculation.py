a80e5e89f653bc2dd363475ec9c52d17a913935d
opokuamaning <princeopokuamaning@gmail.com>
2024-01-22 20:45:43 -0800

    modified 102-manipulation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,25 +4,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            else:
-                result += (a**b)/i
-        except:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
             result = b + a
             break
     return result
-
-#import dis
-#dis.dis(magic_calculation)#!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if (i > a):
-                raise Exception("Too far")
-            else:
-                result += (a ** b) / i
-        except:
-            result = b + a
-            break
-    return (result)

dd3b8380bf226add56f63c3fdb25e75680b0bbd9
opokuamaning <princeopokuamaning@gmail.com>
2024-01-23 16:53:36 -0800

    Added 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private attribute size
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size: size of a side in aquare
+    """
+    def __init__(self, size):
+        """
+        Initialization of square
+        Attributes:
+            size: size of a side of square
+        """
+        self.size = size

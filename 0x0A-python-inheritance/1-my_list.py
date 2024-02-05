31fe30b58abc30f60a84e1f1e05946b4ba2e8353
opokuamaning <princeopokuamaning@gmail.com>
2024-02-05 13:58:14 -0800

    Added 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A method that inherits from a list; has a public instance method to print sorted
+"""
+
+class MyList(list):
+    """
+    methods:
+        print_sorted(self)
+    """
+    def print_sorted(self):
+        print(sorted(self))

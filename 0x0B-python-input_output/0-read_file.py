e67029c3a66928448ac111a2e8755b046a4ec4bd
opokuamaning <princeopokuamaning@gmail.com>
2024-02-06 15:45:50 -0800

    Added 0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a funtion that reads and prints content in a file
+"""
+
+
+
+def read_file(filename=""):
+    """
+    Read and print text froma file
+    """
+    with open(filename, encoding='UTF-8') as f:
+        print(f.read(), end="")

b57dffe02c9b6d0cd97435487c16b436eb16d760
opokuamaning <princeopokuamaning@gmail.com>
2024-02-06 16:11:40 -0800

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ of charachters added
 
 
 def append_write(filename="", text=""):
-    """ appends a string at the end of a text file"""
-    with open(filename, mode='a', encoding='utf-8') as f:
+    """appends a string at the end of a text file"""
+    with open(filename, 'a', encoding='utf-8') as f:
         return(f.write(text))

c0ffbd18c8c58e24f3d0dad0e112a63d69d8eb85
opokuamaning <princeopokuamaning@gmail.com>
2024-02-06 16:48:06 -0800

    checked for pycodestyle

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-a function that creates an Object from a “JSON file
+a function that creates an Object from a “JSON file”
 """
+
+
+import json
+
 
 def load_from_json_file(filename):
     """
     creates an Object from a “JSON file”
     Args: filename
     """
-    import json
-
-    with open(filename, 'r', encoding='utf-8') as f:
-        json.load(f)
+    with open(filename) as f:
+        return json.load(f)

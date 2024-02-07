72b46dc473dd5b1d538d852b3bbaef06b084d611
opokuamaning <princeopokuamaning@gmail.com>
2024-02-06 16:31:54 -0800

    added 4-from_json_string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ a function that returns an object (Python data structure)i
+ represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Args: my_str
+    returns an object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)

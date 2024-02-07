b30ea7d1d6b2acc08e41a2d4d437745fa65c2e79
opokuamaning <princeopokuamaning@gmail.com>
2024-02-06 16:22:19 -0800

    commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,11 +4,12 @@ a function that returns the JSON representation of an object (string)
 """
 
 
+import json
+
+
 def to_json_string(my_obj):
     """
     Args: my_obj
     returns the JSON representation of an object
     """
-    import json
-    
     return json.dumps(my_obj)

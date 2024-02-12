53975dded940006891e9f633fa8f26331d511a53
opokuamaning <princeopokuamaning@gmail.com>
2024-02-12 00:15:08 -0800

    Added setter and getter methods for height, weight, x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,6 @@ Inherits base class
 from models.base import Base
 
 
-
 class Rectangle(Base):
     """
     Rectangle class
@@ -18,4 +17,67 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+    
+    @property
+    def width(self):
+        """width function private"""
 
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width function for setter"""
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height function private"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height function private"""
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x function private"""
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x function setter"""
+
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y function private"""
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y function setter"""
+
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value

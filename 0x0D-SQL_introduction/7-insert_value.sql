8b43bb4be91cd5c9b8380daec99b4265d0aebbfc
opokuamaning <princeopokuamaning@gmail.com>
2024-03-14 02:04:33 -0700

    Modified insert_values.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- a script that inserts a new row in the table first_table
-INSERT INTO first_table (id, name)
-VALUES (89, 'Holberton School');
+-- Inserts a new row in a table
+-- Query to insert a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");

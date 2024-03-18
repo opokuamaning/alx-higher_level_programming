f4759b87ac2e07bb1ab03ebb3f19db19a0c2493b
opokuamaning <princeopokuamaning@gmail.com>
2024-03-18 02:40:13 -0700

    Modified change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- a script that removes all records with a score <= 5 in the table second_table
-DELETE FROM secon_table WHERE score <= 5;
+-- a script that removes all records with a score <= 5 in the table second_tabl
+DELETE FROM second_table WHERE `score` <= 5;

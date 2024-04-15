72135ddf91ecba0a68d8aaba921c6ac3ae9b8132
opokuamaning <princeopokuamaning@gmail.com>
2024-04-15 10:43:54 -0700

    new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
 return all table values (table 'states')
-parameters given script: username, password, database
+parameters given to script: username, password, database
 """
-
 
 import MySQLdb
 from sys import argv
@@ -12,14 +11,15 @@ if __name__ == "__main__":
 
     # connect to database
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
-    # create cursor to exec queries using sql
+    # create cursor to exec queries using SQL
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cursor.fetchall():
         print(row)
     cursor.close()
+    db.close()

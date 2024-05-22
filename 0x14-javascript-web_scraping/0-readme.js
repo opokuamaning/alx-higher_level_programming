b29e65da281da3bbd0b30779bf3c1599ebb2eb09
opokuamaning <princeopokuamaning@gmail.com>
2024-05-22 05:08:06 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 let filename = process.argv[2];
 const fs = require('fs');
 fs.readFile(filename, 'utf8', function (err, data) {
-	if (err) {
-		console.log(err);
-	}else {
-		console.log(data);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
